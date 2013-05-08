d51ccd1f31df05144cd7d42798961d5493fcd00d
Chris <cdesch@gmail.com>
2013-05-08 13:10:55 -0400

    Updating Documentation
    Signed-off-by: Chris <cdesch@gmail.com>

---
+++ LeapPaint/LeapPaintTests/GameManagerTests.h
@@ -0,0 +1,19 @@
+//
+//  GameManagerTests.h
+//  LeapPuzz
+//
+//  Created by cj on 4/24/13.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#import "GameManager.h"
+#import "GameSettings.h"
+/**
+ * Tests the SimplePoint object
+ */
+@interface GameManagerTests : SenTestCase {
+    GameManager* node; /**< gameManager instance  */
+}
+
+@end
