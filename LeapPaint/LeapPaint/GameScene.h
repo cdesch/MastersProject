9e3dd706abd2ae274bd1241268bb7eb40ca1f69b
Chris <cdesch@gmail.com>
2013-05-08 15:05:04 -0400

    GameScene Documentation Update
    Signed-off-by: Chris <cdesch@gmail.com>

--- LeapPaint/LeapPaint/GameScene.h
+++ LeapPaint/LeapPaint/GameScene.h
@@ -11,18 +11,22 @@
 #import "HUDLayer.h"
 #import "GameManager.h"
 #import "LeapObjectiveC.h"
-
 #import "SketchRenderTextureScene.h"
 #import "BackgroundLayer.h"
 #import "ControlsLayer.h"
 
-
+/**
+ GameScene 
+ Initializes and assembles all of the layers and gameobjects into the GameManager
+ 
+ */
 @interface GameScene : CCScene  {
     
 }
-
-
-
+/**
+ Scene initializes each object and assigns interlinking pointers and delegates to each class
+ @return scene for CCDirector to begin running
+ */
 +(CCScene *) scene;
 
 @end
