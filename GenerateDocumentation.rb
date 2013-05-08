184b8fde856868c2c88f386d283f6cfe015b7caa
Chris <cdesch@gmail.com>
2013-05-08 13:00:02 -0400

    Updating Documentation
    Signed-off-by: Chris <cdesch@gmail.com>

--- GenerateDocumentation.rb
+++ GenerateDocumentation.rb
@@ -1,19 +1,28 @@
 #!/usr/bin/ruby
 
+require "rubygems"
+require 'ruby-progressbar'
 rootDir = Dir.pwd
 
 project_names = ["BreakOut", "HelloWorldBlocks", "LeapPaint", "Quartz2DPaint"]
 
+
+progressbar = ProgressBar.create(:format => '%a %B %p%% %t')
 project_names.each do |i|
-	puts rootDir + "/" + i
+	#puts rootDir + "/" + i
+	puts "Generating LaTex Documentation for: " + i  
 	Dir.chdir(rootDir + "/" + i){
 		cmd =  "doxygen doxconfig.cfg"
-	 puts  %x[#{cmd}]
+		#puts  %x[#{cmd}]
+		 %x[#{cmd}]
 	}
+	puts "Generating PDF File from LaTex for: " + i  
 	Dir.chdir(rootDir + "/" + i + "/docs/latex"){
-		cmd =  "make"
-	 puts  %x[#{cmd}]
+		 cmd =  "make"
+		 puts  %x[#{cmd}]
+		 %x[#{cmd}]
 	}
 	
+	progressbar.progress += (project_names.count*2) / 100
 end
 
